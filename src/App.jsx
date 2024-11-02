1365b4e010e981aa38018b41f0d97bcb4d683ad6
Mani Chandan Gujja <152565717+Gujjamanichandan@users.noreply.github.com>
2024-11-02 17:45:11 +0000

    ensuring prioritised symbols

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
+import { useState } from "react";
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState("X");
+
+  function handleSelectSquare() {
+    setActivePlayer((currentPlayer) => (currentPlayer === "X" ? "O" : "X"));
+  }
+
   return (
     <div id="game-container">
-      <ol id="players">
-        <Player initialName="player1" symbol="X" />
-        <Player initialName="player2" symbol="O" />
+      <ol id="players" className="highlight-player">
+        <Player
+          initialName="player1"
+          symbol="X"
+          isActive={activePlayer === "X"}
+        />
+        <Player
+          initialName="player2"
+          symbol="O"
+          isActive={activePlayer === "O"}
+        />
       </ol>
-      <GameBoard />
+      <GameBoard
+        onSelectSquare={handleSelectSquare}
+        activePlayerSymbol={activePlayer}
+      />
     </div>
   );
 }
